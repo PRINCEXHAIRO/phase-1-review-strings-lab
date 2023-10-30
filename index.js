5db2a47a223ff7a4b7ce601b0211a9b5e63f2eb9
Hairo Barnard <Hairo.barnard@gmail.com>
2023-10-30 13:40:47 -0400

    Completed Lab

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // Write your code in this file!
+let currentUser = "Hairo Barnard"
+
+const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`
+
+const upperCaseCurrentUser = currentUser.toUpperCase();
+
+const excitedWelcomeMessage = welcomeMessage.toUpperCase();
+
+const firstInitial = currentUser[0];
+const restOfName = currentUser.slice(1);
+
+
+const shortGreeting = `Welcome, ${firstInitial}!`
+
+
+
+
+
+
+
+
+
+
+
+
